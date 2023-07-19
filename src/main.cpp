341875f1df2cf5400c25fc37e1d73119f80ee7ee
Jean-Martin Albert <jm.albert@gmail.com>
2023-07-19 15:34:45 -0400

    Wifi network scan

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
-#include <TFT_eSPI.h> 
+#include <TFT_eSPI.h>
+#include <WiFi.h>
 
 TFT_eSPI gDisplay;
 
@@ -15,18 +16,45 @@ void setup()
     gDisplay.init();
     gDisplay.setRotation(1);
     gDisplay.fillScreen(TFT_BLUE);
-    gDisplay.setTextSize(3);
+    // gDisplay.setTextSize(3);
 
-    gDisplay.setCursor(10, 30, 2);
-    gDisplay.setTextColor(TFT_SKYBLUE);
-    gDisplay.println("Allo");
+    // gDisplay.setCursor(10, 30, 2);
+    // gDisplay.setTextColor(TFT_SKYBLUE);
+    // gDisplay.println("Allo");
 
-    gDisplay.setCursor(20, 60, 2);
-    gDisplay.setTextColor(TFT_PINK);
-    gDisplay.println("LUCIE");
+    // gDisplay.setCursor(20, 60, 2);
+    // gDisplay.setTextColor(TFT_PINK);
+    // gDisplay.println("LUCIE");
+
+    WiFi.mode(WIFI_STA); // Set as station
+    // WiFi.mode(WIFI_AP);
+    // WiFi.mode(WIFI=_AP_STA);
+    WiFi.disconnect(); // disconnect from network
+    delay(100);
 }
 
 void loop()
 {
+    int n = WiFi.scanNetworks();
+
+    if (n > 0)
+    {
+        char buffer[256];
+        gDisplay.fillScreen(TFT_BLUE);
+        sprintf(buffer, "There are %d networks", n);
+        gDisplay.setCursor(20, 10, 2);
+        gDisplay.setTextColor(TFT_PINK);
+        gDisplay.println(buffer);
+
+        for (uint32_t i = 0; i < n; i++)
+        {
+            char buffer[256];
+            sprintf(buffer, "SSID: %s - Strength %d", WiFi.SSID(i), WiFi.RSSI(i));
+            gDisplay.setCursor(20, 15 * (i + 2), 2);
+            gDisplay.setTextColor(TFT_PINK);
+            gDisplay.println(buffer);
+        }
+    }
+
     // Serial.println("Hello!!");
 }
