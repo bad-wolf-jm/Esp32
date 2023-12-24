c61c35ae84abf323b6742b8156868a5d0ba8ee29
Jean-Martin Albert <jm.albert@gmail.com>
2023-12-24 17:52:01 -0500

    Reactored to remoe templating for led count

--- src/Framebuffer/LedMatrix.h
+++ src/Framebuffer/LedMatrix.h
@@ -2,29 +2,35 @@
 
 #include "LedStripBase.h"
 
-template <uint8_t PANEL_WIDTH, uint8_t PANEL_HEIGHT, uint8_t GRID_WIDTH, uint8_t GRID_HEIGHT, uint8_t DATA_PIN, EOrder RGB_ORDER>
-class LedMatrix : public LedStripBase<PANEL_WIDTH * GRID_WIDTH * PANEL_HEIGHT * GRID_HEIGHT, DATA_PIN, RGB_ORDER>
+template <uint8_t DATA_PIN, EOrder RGB_ORDER>
+class LedMatrix : public LedStripBase<DATA_PIN, RGB_ORDER>
 {
-
   public:
-    static constexpr int Width  = PANEL_WIDTH * GRID_WIDTH;
-    static constexpr int Height = PANEL_HEIGHT * GRID_HEIGHT;
+    int Width;
+    int Height;
 
   private:
-    static constexpr int _numPixelsPerGridRow = Width * PANEL_HEIGHT;
+    int _numPixelsPerGridRow;
+    int _panelWidth;
+    int _panelHeight;
 
-    using LedStripBase<Width * Height, DATA_PIN, RGB_ORDER>::_ledArray;
-    using LedStripBase<Width * Height, DATA_PIN, RGB_ORDER>::_ledCount;
+    using LedStripBase<DATA_PIN, RGB_ORDER>::_ledArray;
+    using LedStripBase<DATA_PIN, RGB_ORDER>::_ledCount;
 
   public:
-    LedMatrix()
-        : LedStripBase<Width * Height, DATA_PIN, RGB_ORDER>()
+    LedMatrix( uint8_t panelWidth, uint8_t panelHeight, uint8_t gridWidth, uint8_t gridHeight )
+        : LedStripBase<DATA_PIN, RGB_ORDER>( panelWidth * gridWidth * panelHeight * gridHeight )
+        , Width{ panelWidth * gridWidth }
+        , Height{ panelHeight * gridHeight }
+        , _panelWidth{ panelWidth }
+        , _panelHeight{ panelHeight }
+        , _numPixelsPerGridRow{ Width * panelHeight }
     {
     }
 
     inline void SetPixel( uint32_t i, uint32_t j, CRGB value )
     {
-        _ledArray[GetIndex(i, j)] = value;
+        _ledArray[GetIndex( i, j )] = value;
     }
 
     void SetPixels( std::vector<CRGB> const &pixelValues )
@@ -36,13 +42,13 @@ class LedMatrix : public LedStripBase<PANEL_WIDTH * GRID_WIDTH * PANEL_HEIGHT * 
     inline int GetIndex( uint32_t i, uint32_t j )
     {
         int index   = 0;
-        int gridRow = i / PANEL_HEIGHT;
+        int gridRow = i / _panelHeight;
 
-        i = i % PANEL_HEIGHT;
+        i = i % _panelHeight;
 
         if( ( j & 0x01 ) == 1 )
-            return _numPixelsPerGridRow * gridRow + ( ( j + 1 ) * PANEL_HEIGHT - ( i + 1 ) );
+            return _numPixelsPerGridRow * gridRow + ( ( j + 1 ) * _panelHeight - ( i + 1 ) );
         else
-            return _numPixelsPerGridRow * gridRow + ( j * PANEL_HEIGHT + i );
+            return _numPixelsPerGridRow * gridRow + ( j * _panelHeight + i );
     }
 };
--- src/Framebuffer/LedStripBase.h
+++ src/Framebuffer/LedStripBase.h
@@ -3,17 +3,17 @@
 #include <FastLED.h>
 #include <vector>
 
-template <int LED_COUNT, uint8_t DATA_PIN, EOrder RGB_ORDER>
+template <uint8_t DATA_PIN, EOrder RGB_ORDER>
 class LedStripBase
 {
   public:
-    LedStripBase( )
-        : _ledCount{ LED_COUNT }
-        , _ledArray{ std::vector<CRGB>( LED_COUNT ) }
+    LedStripBase( int ledCount, float brightness = 5.0f / 255.0f )
+        : _ledCount{ ledCount }
+        , _ledArray{ std::vector<CRGB>( ledCount ) }
 
     {
         _driver.addLeds<WS2812, DATA_PIN, RGB_ORDER>( _ledArray.data(), _ledCount );
-        _driver.setBrightness( 2 );
+        SetBrightness( brightness );
     }
 
     void Clear()
@@ -26,7 +26,13 @@ class LedStripBase
         _driver.show();
     }
 
-  public:
+    void SetBrightness( float brightness )
+    {
+        float clamped = std::max( 0.0f, std::min( brightness, 1.0f ) );
+        _driver.setBrightness( (uint8_t)( clamped * 255.0 ) );
+    }
+
+  protected:
     uint32_t          _ledCount = 0;
     std::vector<CRGB> _ledArray{};
     CFastLED          _driver{};
