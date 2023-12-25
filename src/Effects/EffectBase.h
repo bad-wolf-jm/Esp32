523723f6962526dca594e2eff83f1fbc09ea8fda
Jean-Martin Albert <jm.albert@gmail.com>
2023-12-24 23:43:43 -0500

    Abstract effect class

---
+++ src/Effects/EffectBase.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "Framebuffer/LedStrip.h"
+#include "LedStripRenderer.h"
+
+class LedStripEffectBase
+{
+  public:
+    virtual void Update( float ts ) = 0;
+    virtual void Render( LedStripRenderer &renderer );
+};
