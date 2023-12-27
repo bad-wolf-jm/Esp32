678e8a7b8057f07f24b67a9eb78df77f1b44dd87
Jean-Martin Albert <jm.albert@gmail.com>
2023-12-26 19:06:24 -0500

    Added sound sampling class

---
+++ src/SoundAnalysis.cpp
@@ -0,0 +1,102 @@
+#include "SoundAnalysis.h"
+
+#include <HardwareSerial.h>
+#include <driver/adc.h>
+
+SoundAnalyzer::SoundAnalyzer( i2s_port_t port, int samplingFrequency, i2s_channel_fmt_t channelFormat,
+                              i2s_bits_per_sample_t bitsPerSample, uint8_t bufferCount, int bufferSize )
+    : _port{ port }
+    , _sampleRate{ samplingFrequency }
+    , _bitsPerSample{ bitsPerSample }
+    , _bufferCount{ bufferCount }
+    , _bufferSize{ bufferSize }
+    , _channelFormat{ channelFormat }
+{
+    // clang-format off
+    const i2s_config_t i2s_config = { //
+        .mode                 = (i2s_mode_t)( I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_ADC_BUILT_IN ),
+        .sample_rate          = _sampleRate,
+        .bits_per_sample      = _bitsPerSample,
+        .channel_format       = _channelFormat,
+        .communication_format = I2S_COMM_FORMAT_STAND_I2S,
+        .intr_alloc_flags     = ESP_INTR_FLAG_LEVEL1,
+        .dma_buf_count        = bufferCount,
+        .dma_buf_len          = bufferSize,
+        .use_apll             = false 
+    };
+    // clang-format on
+
+    switch( _bitsPerSample )
+    {
+    case I2S_BITS_PER_SAMPLE_8BIT:
+        _bufferSizeInBytes = _bufferSizeInBytes;
+        break;
+    case I2S_BITS_PER_SAMPLE_16BIT:
+        _bufferSizeInBytes = _bufferSizeInBytes * 2;
+        break;
+    case I2S_BITS_PER_SAMPLE_24BIT:
+        _bufferSizeInBytes = _bufferSizeInBytes * 3;
+        break;
+    case I2S_BITS_PER_SAMPLE_32BIT:
+    default:
+        _bufferSizeInBytes = _bufferSizeInBytes * 4;
+        break;
+    }
+
+    _sampleBuffer = std::vector<uint8_t>( _bufferSizeInBytes );
+
+    _real      = std::vector<double>( _bufferSize );
+    _imaginary = std::vector<double>( _bufferSize );
+
+    ESP_ERROR_CHECK( adc1_config_width( _adcBitsWidth ) );
+    ESP_ERROR_CHECK( adc1_config_channel_atten( _adcChannel, _adcAttenuation ) );
+    ESP_ERROR_CHECK( i2s_driver_install( _port, &i2s_config, 0, NULL ) );
+    ESP_ERROR_CHECK( i2s_set_adc_mode( _adcUnit, _adcChannel ) );
+
+    Serial.println( "I2S driver installed." );
+}
+
+template<typename T>
+inline void FillVector( std::vector<double>& vector, T* values, size_t count)
+{
+    for( size_t i = 0; i < count; i++ )
+        vector[i] = values[i];
+}
+
+void SoundAnalyzer::FillBuffer()
+{
+    size_t bytesRead = 0;
+
+    ESP_ERROR_CHECK( i2s_adc_enable( _port ) );
+    ESP_ERROR_CHECK( i2s_read( _port, (void *)_sampleBuffer.data(), _bufferSizeInBytes, &bytesRead, ( 100 / portTICK_RATE_MS ) ) );
+    ESP_ERROR_CHECK( i2s_adc_disable( _port ) );
+
+    if( bytesRead != _bufferSizeInBytes )
+    {
+        Serial.printf( "Could only read %u bytes of %u in FillBuffer()\n", bytesRead, _bufferSizeInBytes );
+        return;
+    }
+
+    // BUG: _take sample type info account
+    std::fill(_imaginary.begin(), _imaginary.end(), 0.0);
+    switch(_bitsPerSample)
+    {
+    case I2S_BITS_PER_SAMPLE_8BIT:
+        FillVector(_real, (uint8_t*)_sampleBuffer.data(), _bufferSize);
+        break;
+    case I2S_BITS_PER_SAMPLE_16BIT:
+        FillVector(_real, (uint16_t*)_sampleBuffer.data(), _bufferSize);
+        break;
+    case I2S_BITS_PER_SAMPLE_24BIT:
+        // THIS WILL NOT WORK, do template specialization and conversion!!!!
+        FillVector(_real, (uint16_t*)_sampleBuffer.data(), _bufferSize);
+        break;
+    case I2S_BITS_PER_SAMPLE_32BIT:
+    default:
+        FillVector(_real, (uint32_t*)_sampleBuffer.data(), _bufferSize);
+        break;
+    }
+
+    for( int i = 0; i < _bufferSize; i++ )
+        _real[i] = _sampleBuffer[i];
+}
---
+++ src/SoundAnalysis.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <driver/i2s.h>
+#include <vector>
+
+class SoundAnalyzer
+{
+  public:
+    SoundAnalyzer( i2s_port_t port, int samplingFrequency, i2s_channel_fmt_t channelFormat,
+                   i2s_bits_per_sample_t bitsPerSample = I2S_BITS_PER_SAMPLE_16BIT, uint8_t bufferCount = 2, int bufferSize = 512 );
+
+  private:
+    int _bufferSize        = 512;
+    int _sampleRate = 44100;
+
+    i2s_port_t            _port              = I2S_NUM_0;
+    adc_bits_width_t      _adcBitsWidth      = ADC_WIDTH_BIT_12;
+    adc_unit_t            _adcUnit           = ADC_UNIT_1;
+    adc1_channel_t        _adcChannel        = ADC1_CHANNEL_0;
+    adc_atten_t           _adcAttenuation    = ADC_ATTEN_DB_0;
+    i2s_channel_fmt_t     _channelFormat     = I2S_CHANNEL_FMT_RIGHT_LEFT;
+    i2s_bits_per_sample_t _bitsPerSample     = I2S_BITS_PER_SAMPLE_16BIT;
+    uint8_t               _bufferCount       = 2;
+    size_t                _bufferSizeInBytes = 0;
+
+    std::vector<uint8_t> _sampleBuffer;
+
+    std::vector<double> _peaks;
+    std::vector<double> _real;
+    std::vector<double> _imaginary;
+
+  private:
+    void FillBuffer();
+};
